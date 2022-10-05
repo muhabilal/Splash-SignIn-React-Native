3ddfd14433a8709882f91e1e2db9da1d8705322a
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-05 15:09:22 +0500

    navigator runing form

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -8,14 +8,14 @@ const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name='Splash Screen' component={Splash}/>
-    //     {/* <Stack.Screen name="SignInScreen"  component={SignInScreen}/> */}
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Splash Screen' component={Splash} />
+        <Stack.Screen name="SignInScreen" component={SignInScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     // <Splash/>
-    <SignInScreen/>
+    // <SignInScreen/>
   )
 }
 
--- Screens/SignInScreen.js
+++ Screens/SignInScreen.js
@@ -1,18 +1,21 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
-
+// import { AntDesign } from 'react-native-vector-icons/AntDesign'
 const SignInScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.text_header}>Welcome!</Text>
       </View>
-          <View style={styles.footer}>
-              <Text style={styles.text_footer}>Email</Text>
-              <View style={styles.action}>
-                <TextInput/>
-              </View>
-          </View>
+      <View style={styles.footer}>
+        <Text style={styles.text_footer}>Email</Text>
+        <View style={styles.action}>
+          {/* <AntDesign name="home" size={24} color="black" /> */}
+          <TextInput placeholder='Your Email'
+            autoCapitalize='none'
+          />
+        </View>
+      </View>
     </View>
   )
 }
@@ -20,28 +23,32 @@ const SignInScreen = () => {
 export default SignInScreen
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#009387',
-    },
-    header:{
-        flex:1,
-        justifyContent:'flex-end',
-        paddingHorizontal: 20,
-        paddingBottom: 50,
+  container: {
+    flex: 1,
+    backgroundColor: '#009387',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
 
-    },
-    footer:{
-        flex:3,
-        backgroundColor:"#fff",
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-    },
-    text_header:{
-        color:"white",
-        fontWeight:"bold",
-        fontSize:50,
-    }
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 50,
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18,
+  },
 })
